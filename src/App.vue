2d80af5df7ed6fd9331bbd7e890e0410fa8b7fab
Jordan <jordan-souchez@hotmail.fr>
2021-11-19 15:55:23 +0100

    feat(Homepage) : Styling homepage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <homepage msg="Welcome to Vue.js App" />
+  <Homepage/>
 </template>
 
 <script>
@@ -14,14 +14,3 @@ export default {
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
