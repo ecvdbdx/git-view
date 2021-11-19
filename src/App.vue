b8d003f53bf5e67b0373c5bc904c53d165d1b9ab
Kewey <35737881+Kewey@users.noreply.github.com>
2021-11-19 17:58:29 +0100

    feat(homepage) : Open file browser and redirect if selected folder is a Git folder (#11)
    * feat : Add WindiCSS
    
    * fix : remove windi for webpack
    
    * feat : Add WindiCSS
    
    * feat(Homepage) : Config Windi
    
    * feat(Homepage) : Styling homepage
    
    * feat : Add new icon
    
    * feat: add routing
    
    * feat(folderReader): add folderReader logics (#13)
    
    * feat(folderReader): add folderReader logics
    
    * fix(folderReader): naming and esm import
    
    Co-authored-by: teyz <contact@bastienrigaud.fr>
    Co-authored-by: Clément Ollivier <clement.o2p@gmail.com>
    
    * feat : Add WindiCSS
    
    * fix : remove windi for webpack
    
    * feat : Add WindiCSS
    
    * feat(Homepage) : Config Windi
    
    * feat(Homepage) : Styling homepage
    
    * feat: add routing
    
    * feat : Add new icon
    
    * feat(Homepage) : Link folderService
    
    * fix(types): use const
    
    Co-authored-by: Tristan Autin <autintristan@outlook.fr>
    Co-authored-by: Bastien R <contact.teyz@gmail.com>
    Co-authored-by: teyz <contact@bastienrigaud.fr>
    Co-authored-by: Clément Ollivier <clement.o2p@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Vue.js App" />
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import "windi.css";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
