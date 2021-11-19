1a99e2f588b441295044bd8e55ad365965f4a51c
Jordan <jordan-souchez@hotmail.fr>
2021-11-19 14:34:33 +0100

    feat(Homepage) : Config Windi

---
+++ src/components/Homepage.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="bg-gray-600">Bienvenu sur Git View</h1>
+  <button class="p-4">Selectionner un dossier</button>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
