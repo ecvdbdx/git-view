231cdd80c99bfcd58ebd8d48bf974f17d1e9a388
teyz <contact@bastienrigaud.fr>
2021-11-26 16:31:09 +0100

    fix(gitParser.spec): update test

--- src/services/__tests__/gitParser.spec.js
+++ src/services/__tests__/gitParser.spec.js
@@ -1,30 +1,32 @@
 import GitReader from '../gitReader';
 
-const gitIncommingCommits = `dff4044$$fix(contact, projects, about): responsive$$Sun Nov 21 19:20:35 2021 +0100$$teyz
-430fdcf$$feat(img): optimize pagespeed$$Sun Nov 21 07:30:52 2021 +0100$$teyz
-68dcd52$$feat(projects): add projects page features$$Sun Nov 21 07:15:10 2021 +0100$$teyz`;
-
-test('Format commit list from .git folder', () => {
-  const gitReader = new GitReader();
-
-  expect(gitReader.parser(gitIncommingCommits)).toStrictEqual([
-    {
-      sha: 'dff4044',
-      message: 'fix(contact, projects, about): responsive',
-      date: 'Sun Nov 21 19:20:35 2021 +0100',
-      author: 'teyz',
-    },
-    {
-      sha: '430fdcf',
-      message: 'feat(img): optimize pagespeed',
-      date: 'Sun Nov 21 07:30:52 2021 +0100',
-      author: 'teyz',
-    },
-    {
-      sha: '68dcd52',
-      message: 'feat(projects): add projects page features',
-      date: 'Sun Nov 21 07:15:10 2021 +0100',
-      author: 'teyz',
-    },
-  ]);
+describe('Given a string of commit', () => {
+  const gitIncommingCommits = `dff4044$$fix(contact, projects, about): responsive$$Sun Nov 21 19:20:35 2021 +0100$$teyz
+  430fdcf$$feat(img): optimize pagespeed$$Sun Nov 21 07:30:52 2021 +0100$$teyz
+  68dcd52$$feat(projects): add projects page features$$Sun Nov 21 07:15:10 2021 +0100$$teyz`;
+  describe('When the string is correct', () => {
+    it('Should return an Array of commit', () => {
+      const gitReader = new GitReader();
+      expect(gitReader.parser(gitIncommingCommits)).toStrictEqual([
+        {
+          sha: 'dff4044',
+          message: 'fix(contact, projects, about): responsive',
+          date: 'Sun Nov 21 19:20:35 2021 +010',
+          author: 'teyz',
+        },
+        {
+          sha: '430fdcf',
+          message: 'feat(img): optimize pagespeed',
+          date: 'Sun Nov 21 07:30:52 2021 +0100',
+          author: 'teyz',
+        },
+        {
+          sha: '68dcd52',
+          message: 'feat(projects): add projects page features',
+          date: 'Sun Nov 21 07:15:10 2021 +0100',
+          author: 'teyz',
+        },
+      ]);
+    });
+  });
 });
