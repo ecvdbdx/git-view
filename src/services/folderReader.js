7b826e05c86ccb7dc8d5eeda00789157881db718
Bastien R <contact.teyz@gmail.com>
2021-11-26 13:46:57 +0100

    feat(folderReader): add constants for events call (#14)
    * feat(folderReader): add folderReader logics
    
    * feat(foldeerReader): add constants for events call
    
    * fix(folderReader): naming and esm import
    
    * fix(folderReader): naming and return
    
    * fix(background): remove undefined constant
    
    Co-authored-by: teyz <contact@bastienrigaud.fr>
    Co-authored-by: Clément Ollivier <clement.o2p@gmail.com>
    Co-authored-by: Felix <bouveret.felix@gmail.com>

--- src/services/folderReader.js
+++ src/services/folderReader.js
@@ -1,17 +1,17 @@
-"use strict";
+'use strict';
 
-import { dialog } from "electron";
-import { access } from "fs/promises";
+import { dialog } from 'electron';
+import { access } from 'fs/promises';
 
 class folderReader {
   constructor(electronWindow) {
     this.window = electronWindow;
     this.options = {
-      title: "Choose a github project",
-      defaultPath: "D:\\electron-app",
-      buttonLabel: "Select this folder",
-      filters: [{ name: "All Files", extensions: ["*"] }],
-      properties: ["openDirectory"],
+      title: 'Choose a github project',
+      defaultPath: 'D:\\electron-app',
+      buttonLabel: 'Select this folder',
+      filters: [{ name: 'All Files', extensions: ['*'] }],
+      properties: ['openDirectory'],
     };
   }
 
@@ -24,12 +24,11 @@ class folderReader {
 
   checkIsGitFolder(folderPath) {
     return access(`${folderPath}/.git`)
-      .then(() => {
-        return {folderPath, isGit: true};
-      })
-      .catch(() => {
-        return {error : "This folder does not contain a git folder", isGit: false};
-      });
+      .then(() => ({ folderPath, isGit: true }))
+      .catch(() => ({
+        error: 'This folder does not contain a git folder',
+        isGit: false,
+      }));
   }
 }
 export default folderReader;
---
+++ utils/constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+  GET_FOLDER_PATH_EVENT: "getFolderPath-event",
+  GET_FOLDER_PATH_REPLY: "getFolderPath-reply",
+};
