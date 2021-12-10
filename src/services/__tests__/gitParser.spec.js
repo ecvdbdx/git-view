6ab65952201767ae557594941d31a5e4fd0fa539
Tristan-Autin <55452214+Tristan-Autin@users.noreply.github.com>
2021-12-10 14:50:21 +0100

    added mocks and front end for last ten commits (#20)
    * added mocks and front end for last ten commits
    
    * feat(gitView) : added mocks and last ten commits
    
    * fix pr
    
    * feat(gitView): call back end
    
    * feat(gitReader git-view): add HEAD detection and reverse commits order
    
    * fix:(unit test): update GitReader test
    
    Co-authored-by: Clément Ollivier <clement.o2p@gmail.com>

--- src/services/__tests__/gitParser.spec.js
+++ src/services/__tests__/gitParser.spec.js
@@ -3,7 +3,7 @@ import GitReader from '../gitReader';
 describe('Given a string of commit', () => {
   const gitReader = new GitReader();
   const gitIncommingCommits = `dff4044$$fix(contact, projects, about): responsive$$Sun Nov 21 19:20:35 2021 +0100$$teyz
-  430fdcf$$feat(img): optimize pagespeed$$Sun Nov 21 07:30:52 2021 +0100$$teyz
+  430fdcf$$feat(img): optimize pagespeed$$Sun Nov 21 07:30:52 2021 +0100$$teyz$$ (HEAD)
   68dcd52$$feat(projects): add projects page features$$Sun Nov 21 07:15:10 2021 +0100$$teyz`;
 
   it('Should return a Promise', () => {
@@ -13,22 +13,25 @@ describe('Given a string of commit', () => {
   it('Should return an Array of commit', () => {
     expect(gitReader.parser(gitIncommingCommits)).toStrictEqual([
       {
-        sha: 'dff4044',
-        message: 'fix(contact, projects, about): responsive',
-        date: 'Sun Nov 21 19:20:35 2021 +0100',
+        sha: '68dcd52',
+        message: 'feat(projects): add projects page features',
+        date: 'Sun Nov 21 07:15:10 2021 +0100',
         author: 'teyz',
+        isHead: false,
       },
       {
         sha: '430fdcf',
         message: 'feat(img): optimize pagespeed',
         date: 'Sun Nov 21 07:30:52 2021 +0100',
         author: 'teyz',
+        isHead: true,
       },
       {
-        sha: '68dcd52',
-        message: 'feat(projects): add projects page features',
-        date: 'Sun Nov 21 07:15:10 2021 +0100',
+        sha: 'dff4044',
+        message: 'fix(contact, projects, about): responsive',
+        date: 'Sun Nov 21 19:20:35 2021 +0100',
         author: 'teyz',
+        isHead: false,
       },
     ]);
   });
--- src/services/gitReader.js
+++ src/services/gitReader.js
@@ -1,18 +1,22 @@
 import { exec } from 'child_process';
 
 export default class GitReader {
-  constructor() {}
-
   parser(gitString) {
-    return gitString.split('\n').map((commit) => {
-      const commitArray = commit.trim().split('$$');
-      return {
-        sha: commitArray[0],
-        message: commitArray[1],
-        date: commitArray[2],
-        author: commitArray[3],
-      };
-    });
+    return gitString
+      .split('\n')
+      .map((commit) => {
+        const [sha, message, date, author, refNames] = commit
+          .trim()
+          .split('$$');
+        return {
+          sha,
+          message,
+          date,
+          author,
+          isHead: refNames ? refNames.includes('(HEAD)') : false,
+        };
+      })
+      .reverse();
   }
 
   /**
@@ -23,7 +27,7 @@ export default class GitReader {
   getGitLogs(path, nbCommit = 10) {
     return new Promise((resolve, reject) => {
       exec(
-        `git log HEAD --max-count=${nbCommit} --pretty='format:%h$$%s$$%cd$$%an'`,
+        `git log origin --max-count=${nbCommit} --pretty='format:%h$$%s$$%cd$$%an$$%d'`,
         { cwd: path },
         (err, stdout) => {
           if (err) {
