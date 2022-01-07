b7fd28c249036ff0b6ba2594a95fe98e868d7a78
Felix <bouveret.felix@gmail.com>
2022-01-07 14:15:16 +0100

    fix(eslint): set no-unused-var to warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
     'prettier/prettier': ['warn', { singleQuote: true }],
+    'no-unused-vars': 'warn',
   },
 };
