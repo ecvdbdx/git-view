975c540652b12f61127fcf64a1ba5bb2b35fd6e8
Lahaxe Hugo <lahaxeh@gmail.com>
2022-02-04 16:13:48 +0100

    feat: displaying commit details

--- src/modules/GitView/components/CommitDetails.vue
+++ src/modules/GitView/components/CommitDetails.vue
@@ -4,16 +4,36 @@
     class="flex flex-col border-t-green-400 border-solid border-t-width-2"
   >
     <DsButton class="mx-4 mt-4">Close</DsButton>
+    <p>{{ files.details }}</p>
     <ul
       class="flex flex-col justify-center text-xs flex-shrink-0 p-4 flex-grow-0"
-    ></ul>
+    >
+      <li
+        v-for="([filename, affected_line, stat], index) in files.files"
+        :key="index"
+      >
+        <span>{{ filename }}</span>
+        <span>{{ affected_line }}</span>
+        <span>{{ stat }}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { computed } from '@vue/reactivity';
+
 export default {
   name: 'CommitDetails',
-  props: {},
-  setup() {},
+  props: {
+    files: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    const details = computed(() => props.files.details);
+    const files = computed(() => props.files.files);
+  },
 };
 </script>
